c03da570b8326a788ca5ebc0bc4030ea034978ae
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-02-23 14:50:46 +0800

    added test and added preview

--- quantum-tick/src/main/java/com/group1/App.java
+++ quantum-tick/src/main/java/com/group1/App.java
@@ -1,13 +1,26 @@
 package com.group1;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+public class App {
+    public static void main( String[] args ){
+        createAndShowGUI();
     }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("QuantumTick - Test App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null); 
+
+        JLabel label = new JLabel("QuantumTick is successfully running!", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 16));
+        
+        frame.add(label);
+        frame.setVisible(true);
+    }
+
 }
