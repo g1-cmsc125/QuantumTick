22689df84a3e78d1d662ef65459f6b5ee8da4d5d
ausdotsn50 <ausdotsn50@gmail.com>
2026-02-27 21:14:55 +0800

    fix: separation between pages, scripts, and styles

--- quantum-tick/src/main/resources/script.js
+++
@@ -1,7 +0,0 @@
-// Loading the whole DOM
-document.addEventListener('DOMContentLoaded', () => {
-    const startBtn = document.getElementById('btn-start');
-        startBtn.addEventListener('click', () => {
-            window.location.href = 'pages/start.html'; 
-        });
-});
---
+++ quantum-tick/src/main/resources/scripts/script.js
@@ -0,0 +1,12 @@
+// Loading the whole DOM
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.getElementById('btn-start');
+    startBtn.addEventListener('click', () => {
+        window.location.href = 'pages/start.html'; 
+    });
+
+    const hiwBtn = document.getElementById('btn-how');
+    hiwBtn.addEventListener('click', () => {
+        window.location.href = 'pages/hiw.html'; 
+    });
+});
